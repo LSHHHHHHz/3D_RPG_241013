78bfb8d36a7b3e49d132dd1f73fef33a4aa72d0b
Hoon <opunho118@naver.com>
2024-10-18 00:07:48 +0900

    구매 팝업 구현

---
+++ Assets/Scrips/UI/Popup/ShopBuyPopupUI.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopBuyPopupUI : MonoBehaviour
+{
+    [SerializeField] Image itemImage;
+    [SerializeField] Text itemName;
+    [SerializeField] Text totalPrice;
+    [SerializeField] Text count;
+    [SerializeField] Button increaseCountButton;
+    [SerializeField] Button decreaseCountButton;
+    [SerializeField] Button buyItemButton;
+    [SerializeField] Button closePopupButton;
+    private int currentCount = 1;
+    private int itemPrice;
+    private string dataID;
+    private void Awake()
+    {
+        increaseCountButton.onClick.AddListener(() =>
+        {
+            IncreaseCount();
+        });
+        decreaseCountButton.onClick.AddListener(() =>
+        {
+            DecreaseCount();
+        });
+        buyItemButton.onClick.AddListener(() =>
+        {
+            BuyItem(dataID, currentCount);
+        });
+        closePopupButton.onClick.AddListener(() =>
+        {
+            gameObject.SetActive(false);
+        });
+    }
+
+    public void SetData(GameDBEntity db)
+    {
+        dataID = db.dataID;
+        itemImage.sprite = Resources.Load<Sprite>(db.iconPath);
+        itemName.text = db.name;
+        itemPrice = db.price;
+        currentCount = 1;
+        UpdateUI();
+    }
+    private void IncreaseCount()
+    {
+        currentCount++;
+        UpdateUI();
+    }
+    private void DecreaseCount()
+    {
+        if (currentCount > 1)
+        {
+            currentCount--;
+            UpdateUI();
+        }
+    }
+    private void BuyItem(string id, int count)
+    {
+        for (int i = 0; i < GameData.instance.itemInventoryData.slotDatas.Count; i++)
+        {
+            if (string.IsNullOrEmpty(GameData.instance.itemInventoryData.slotDatas[i].dataID))
+            {
+                GameData.instance.itemInventoryData.slotDatas[i].SetData(id, count);
+                break;
+            }
+        }
+    }
+    private void UpdateUI()
+    {
+        count.text = currentCount.ToString();
+        totalPrice.text = (currentCount * itemPrice).ToString();
+    }
+}
--- Assets/Scrips/UI/SlotUI/ShopSlotUI.cs
+++ Assets/Scrips/UI/SlotUI/ShopSlotUI.cs
@@ -8,20 +8,35 @@ public class ShopSlotUI : MonoBehaviour
     [SerializeField] Text itemName;
     [SerializeField] Text iemDescription;
     [SerializeField] Text itemPrice;
+    [SerializeField] GameObject shopBuyPopupPrefab;
+    ShopBuyPopupUI shopBuyPopupUI;
+    GameDBEntity db;
     private void Awake()
     {
         Button button = GetComponent<Button>();
         button.onClick.AddListener(() =>
         {
-            Debug.Log("ÆË¾÷ ¶ç¿ì±â");
+            OpenShopBuyPopup();
         });
     }
     public void SetData(string dataID)
     {
-        GameDBEntity db = GameManager.instance.gameDB.GetProfileDB(dataID);
+        db = GameManager.instance.gameDB.GetProfileDB(dataID);
         itemImage.sprite = Resources.Load<Sprite>(db.iconPath);
         itemName.text = db.name;
         iemDescription.text = db.description;
         itemPrice.text = db.price.ToString();
+    }
+    void OpenShopBuyPopup()
+    {
+        if(shopBuyPopupUI == null)
+        {
+            shopBuyPopupUI = Instantiate(shopBuyPopupPrefab, transform).GetComponent<ShopBuyPopupUI>();
+        }
+        else
+        {
+            shopBuyPopupUI.gameObject.SetActive(true);
+        }
+        shopBuyPopupUI.SetData(db);
     }
 }
